2e425520cff25635f98c254c2b77ce3c05c3d27f
Grosbelli112 <gustavogrosbelli11@gmail.com>
2024-09-11 20:00:00 -0300

    Atv for

---
+++ src/Atividade_2_Contagem_Regressiva.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Atividade_2_Contagem_Regressiva {
+    public static void main(String[] args) {
+        Scanner subtracao = new Scanner(System.in);
+        System.out.println("Digite um valor para subtrair até 0: ");
+        int num  = subtracao.nextInt();
+        while (num != 0) {}
+
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -4,23 +4,14 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner num = new Scanner(System.in);
-        int[] tabuada = new int[10];
+        int[] tabuada = new int[11];
         System.out.println("digite um número de 1 até 10: ");
         int num1 = num.nextInt();
-        int soma = 0;
-        for (int i = 0; i < 10; i++) {
-            soma += i;
 
-            tabuada[0] = num1 * 1;
-            tabuada[1] = num1 * 2;
-            tabuada[2] = num1 * 3;
-            tabuada[3] = num1 * 4;
-            tabuada[4] = num1 * 5;
-            tabuada[5] = num1 * 6;
-            tabuada[6] = num1 * 7;
-            tabuada[7] = num1 * 8;
-            tabuada[8] = num1 * 9;
-            tabuada[9] = num1 * 10;
+        for (int i = 1; i < 11; i++) {
+
+            tabuada[i] = num1 * i;
+
             System.out.println("o resultado da sua tabuada é: " + tabuada[i]);
 
         }
