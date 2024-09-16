0ca7210921ceb2ab75c08a2785dd087b06a2a3a4
GustavoGrosbelli <gustavogrosbelli26@gmail.com>
2024-09-16 13:57:07 -0300

    atv2

--- src/Atividade_2_Contagem_Regressiva.java
+++ src/Atividade_2_Contagem_Regressiva.java
@@ -3,20 +3,14 @@ import java.util.Scanner;
 public class Atividade_2_Contagem_Regressiva {
     public static void main(String[] args) {
 
-        Scanner subtracao = new Scanner(System.in);
-        System.out.println("Digite um valor para subtrair até 0 : ");
-        int num = subtracao.nextInt();
+        Scanner entrada = new Scanner(System.in);
 
-        int[] contagem = new int[num];
-        int i = num;
+        System.out.println("Digite o número  para fazer uma contagem regressiva até zero: ");
+        int num = entrada.nextInt();
 
-        System.out.println("A subtração até 0 do número desejadp é : ");
-
-        while (i > 0) {
-            System.out.println(i);
-            contagem[0] = i;
-            i--;
+        while (num >= 0) {
+            System.out.println(num);
+            num--;
         }
-
     }
 }
